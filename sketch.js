f7b7ebcef538c6e1ed758727c1f785eaec3eb287
aadityabarwar <69247172+aadityabarwar@users.noreply.github.com>
2020-08-23 10:08:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var Monkey ;
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+var monkey = createSprite(80,310,1,1);
+
+
+
+}
+
+function draw() {
+  background(220);
+
+drawsprites();
+
+}
